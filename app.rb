983bbf4f97430284f8afaddb7f990004d0fe052c
cjkautsky <cjkautsky@gmail.com>
2020-02-26 03:09:09 +0000

    Second updates

--- app.rb
+++ app.rb
@@ -7,12 +7,12 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
 # enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "37eea36982ddc3f378c0a1bcf299781a"
 
 get "/" do
   view "ask"
 end
 
 get "/news" do
-  view "news"
+    view "news"
 end
